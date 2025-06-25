c13db9effa2a151ac24a65e180e3ff3900ecdc56
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-25 12:55:59 +0100

    Refactor Header component structure and update CSS styles for improved layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 export const Header = () => {
     return (
 
-        <>
-
-            <nav className="nav-bar">
-                <img src="public/images/rep_portuguesa_white.png"/>
-                <ul>
-                    <li>Contacto</li>
-                    <li>Ajuda</li>
-                    <li>
-                        <button>Login</button>
-                    </li>
-                </ul>
-
-            </nav>
-
-        </>
+        <div className="nav-container">
+            <div className="nav-logo">
+                <img src="public/images/rep_portuguesa_white.png" alt="Logo" height="100"/>
+            </div>
+            <div className="nav-navigation">
+                <nav className="nav-bar">
+                    <ul>
+                        <li>Contacto</li>
+                        <li>Ajuda</li>
+                        <li>
+                            <button>Login</button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     );
 };
