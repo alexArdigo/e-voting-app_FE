972a5728c0bee2d5528dfe8c2af6192e5e0d0b53
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-25 12:27:56 +0100

    Add navigation bar component and update App structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+    return (
+
+        <>
+
+            <nav className="nav-bar">
+                <img src="public/images/rep_portuguesa_white.png"/>
+                <ul>
+                    <li>Contacto</li>
+                    <li>Ajuda</li>
+                    <li>
+                        <button>Login</button>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </>
+    );
+};
