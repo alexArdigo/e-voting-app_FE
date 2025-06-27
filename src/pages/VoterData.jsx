fdbf3c9dbb21de57e90f65c77d73241b621f2f34
Fernando Rocha <feara@iscte-iul.pt>
2025-06-27 15:48:02 +0100

    Display user-specific voter data in VoterData page with context integration.

--- src/pages/VoterData.jsx
+++ src/pages/VoterData.jsx
@@ -4,12 +4,25 @@ import {useUserContext} from "../services/UserContext.jsx";
 const VoterData = () => {
 
     const context= useUserContext();
+    const user = context.user;
 
   return (
     <div>
         <Header/>
         <h1>Voter Data</h1>
-        <p>This page will display voter data.</p>
+        <div>
+            <p><strong>Nome:</strong> {user?.nome}</p>
+            <p><strong>Data Nascimento:</strong> {user?.nascimento}</p>
+            <p><strong>Contacto telefónico:</strong> {user?.telefone}</p>
+            <p><strong>NIF:</strong> {user?.nif}</p>
+        </div>
+
+        <div>
+            <p><strong>Círculo Eleitoral:</strong> {user?.circulo}</p>
+            <p><strong>Freguesia:</strong> {user?.freguesia}</p>
+            <p><strong>Elegível:</strong> {user?.elegivel ? "Sim" : "Não"}</p>
+            <p><strong>Verificado:</strong> {user?.verificado ? "Sim" : "Não"}</p>
+        </div>
         <input
             type="radio"
             id="dataConfirmation"
