c5975cb3332b0d81dbc3df970780b41b0e45f032
Fernando Rocha <feara@iscte-iul.pt>
2025-06-27 17:10:44 +0100

    Add FAQ page and integrate it into routes within App component.

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,23 +1,54 @@
 import Header from "../componentsAPP/Header.jsx";
+import {useEffect, useState} from "react";
+import api from "../services/api.jsx";
 
 const Faq = () => {
+
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        async function fetchComments() {
+            try {
+                const comentarios = await api.get(`/comments`);
+                setComments(comentarios.data);
+            } catch (error) {
+                alert("Erro ao carregar comentários.");
+            }
+        }
+
+        fetchComments();
+    }, []);
+
     return (
         <>
             <Header />
             <main className="general-container">
                 <h1>FAQ</h1>
-                <section>
-                    <h2>Como posso comprar bilhetes?</h2>
-                    <p>Os bilhetes podem ser adquiridos online através do nosso site ou em pontos de venda autorizados.</p>
+
+                <section className="comment-section">
+                    <h2>Comentários</h2>
+                    <div className="comments-list">
+                        {comments.map((c, index) => {
+                            return (
+                                <HelpComment
+                                    key={index}
+                                    id={c.id}
+                                    comment_text={c.comment}
+                                    pub_datetime={c.dateTime}
+                                    likes={c.likesCount}
+                                />
+                            );
+                        })}
+                    </div>
                 </section>
+
                 <section>
-                    <h2>Quais são os horários dos concertos?</h2>
-                    <p>Os horários dos concertos estão disponíveis na nossa página de programação.</p>
+                    <h2>Tem mais alguma pergunta? Escreva suas dúvidas abaixo.</h2>
+                    <form>
+
+                    </form>
                 </section>
-                <section>
-                    <h2>Há estacionamento disponível no local?</h2>
-                    <p>Sim, há estacionamento disponível, mas recomendamos o uso de transportes públicos para evitar congestionamentos.</p>
-                </section>
+
             </main>
         </>
     );
