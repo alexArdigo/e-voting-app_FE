71a9d5deca4466d11a5796ac52f717b2898586b5
Fernando Rocha <feara@iscte-iul.pt>
2025-06-27 23:31:21 +0100

    Redesign `Home` page layout with new `overlay`, logo, and vote button styling; update CSS for improved structure and responsiveness.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-
-
 const Home = () => {
     return (
-        <main className={"main-container"}>
-            <button>Eu Voto</button>
+        <main className="main-container">
+            <div className="overlay">
+                <img src="/images/rep_portuguesa_white.png" alt="Logo República Portuguesa" className="main-logo" />
+                <button className="vote-button">EU VOTO</button>
+            </div>
         </main>
     );
-}
+};
 
 export default Home;
