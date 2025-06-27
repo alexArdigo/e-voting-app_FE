75b3b11ab2ae11420d6141448259d122a0e7021c
Fernando Rocha <feara@iscte-iul.pt>
2025-06-27 18:48:42 +0100

    Update `HelpComment` date formatting and add `PageDefault` component with `Header` integration

--- src/Components/HelpComment.jsx
+++ src/Components/HelpComment.jsx
@@ -2,7 +2,7 @@ const HelpComment = ({ comment_text, pub_datetime, likes, answer }) => {
     return (
         <div className="comment">
             <p className="comment-text">{comment_text}</p>
-            <p className="comment-date">{pub_datetime}</p>
+            <p><em>{new Date(pub_datetime).toLocaleString()}</em></p>
             <p className="comment-likes">Likes: {likes}</p>
             {answer && (
                 <div className="admin-answer">
