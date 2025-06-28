3e1b1eefcb30beffe02bb8fef71eb1cc9f6e2363
Fernando Rocha <feara@iscte-iul.pt>
2025-06-28 11:01:24 +0100

    Add form wrapper to `Home` page vote button for improved accessibility and functionality.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 const Home = () => {
+
+
+
     return (
         <main className="main-container">
             <div className="overlay">
                 <img src="/images/rep_portuguesa_white.png" alt="Logo República Portuguesa" className="main-logo" />
+                <form action="/voter-data">
                 <button className="vote-button">EU VOTO</button>
+                </form>
             </div>
         </main>
     );
