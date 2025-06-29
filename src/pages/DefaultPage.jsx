db0dea6a7500c85e4ad072d004bf0727e2b28494
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-29 15:21:30 +0100

    Add half-logo class for improved positioning and styling in DefaultPage

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -4,9 +4,7 @@ import MainLayout from "./MainLayout.jsx";
 const DefaultPage = () => (
 
     <MainLayout>
-
-        <img src="./images/legislativas_halfLogo.png" alt="half-logo" style={{height: '300px'}} />
-
+        <img className="half-logo" src="./images/legislativas_halfLogo.png" alt="half-logo"/>
     </MainLayout>
 );
 
