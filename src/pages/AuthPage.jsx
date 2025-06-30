b180d40d7add35aae52ff2a123f0e7347c6f2ad8
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-06-30 13:00:11 +0100

    Add navigation to AuthPage and update voting button in ConfirmElectionPage

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import MainLayout from "../layouts/MainLayout.jsx";
+import {useNavigate} from "react-router-dom";
 
 const AuthPage = () => {
+
+    const navigate = useNavigate();
+
     return (
         <MainLayout>
             <div className="auth-container">
@@ -12,9 +16,9 @@ const AuthPage = () => {
                 <div className="form-section">
                     <h2>Faça sua autenticação com a <strong>Chave Móvel Digital.</strong></h2>
 
-                    <button className="vote-button">
+                    <button className="vote-button" onClick={() => navigate("/election")} >
                         Chave Móvel Digital
-                    </button>
+                    </button> {/* <<<<< este navigate só esta aqui em placeholder ate haver a ligação com a chave movel digital*/}
                 </div>
             </div>
         </MainLayout>
