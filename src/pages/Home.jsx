b7c5d4f468e912ce1fec488a5f71258180fba735
Fernando Rocha <feara@iscte-iul.pt>
2025-06-30 13:55:55 +0100

    Update Home page button class and styles; increase home logo size for better visibility

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <main className="home-container">
             <div className="overlay">
                 <img src="/images/rep_portuguesa_white.png" alt="Logo República Portuguesa" className="home-logo" />
-                <button className="vote-button" onClick={() => navigate("/instructions")}>EU VOTO</button>
+                <button className="home-button" onClick={() => navigate("/instructions")}>EU VOTO</button>
             </div>
 
             <div className={"corner"}>
