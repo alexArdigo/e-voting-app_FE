3f9711983e8153b4c3265ba64e72437fc4c8d912
Fernando Rocha <feara@iscte-iul.pt>
2025-06-30 10:31:19 +0100

    Refactor component import paths for consistency; replace `arco.png` with `legislativas_halfLogo.png` in Instructions page.

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainLayout from "../layouts/MainLayout.jsx";
-import HalfLogo from "./HalfLogo.jsx";
+import HalfLogo from "../components/common/HalfLogo.jsx";
 import StyledContainer from "../components/specific/StyledContainer.jsx";
 import "../css/App.css"
 
