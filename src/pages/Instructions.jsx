ea0181b4f9919396e1cb55659c306197494f447a
Fernando Rocha <feara@iscte-iul.pt>
2025-07-01 10:11:22 +0100

    Update `package-lock.json` to reflect project rebranding (`evoting_client` → `e-voting-app_fe`) and dependencies cleanup.

--- src/pages/Instructions.jsx
+++ src/pages/Instructions.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { useNavigate } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout.jsx";
 import StyledContainer from "../components/specific/StyledContainer.jsx";
