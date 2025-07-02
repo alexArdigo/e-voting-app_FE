10f4a93c495f4758b8d6a3fa6885e8fa1c815b38
rita_santos <rassa2@iscte-iul.pt>
2025-07-02 11:57:00 +0100

    Refactor Register component layout and improve image display

--- src/pages/Viewer/Register.jsx
+++ src/pages/Viewer/Register.jsx
@@ -5,7 +5,6 @@ import './viewer.css';
 import MainLayout from "../../layouts/MainLayout";
 
 
-
 const Register = () => {
 
     const [inputs, setInputs] = useState({
@@ -50,28 +49,29 @@ const Register = () => {
 
     return (
         <MainLayout>
-                <div className="user-info-container">
+            <div className="user-info-container">
+                <img src="./../public/images/legislativas-viewer.png" alt="Register" className="register-image"/>
                     <h3>Formulário de Registo</h3>
 
 
                     <form onSubmit={handleSubmit}>
 
-                        <input id="username" type="text" name="username" placeholder="Nome de utilizador" value={inputs.username}
+                        <input id="username" type="text" name="username" placeholder="Nome de utilizador"
+                               value={inputs.username}
                                onChange={handleInputs}/>
                         <input id="password" type="password" name="password" placeholder="Palavra-passe"
                                value={inputs.password} onChange={handleInputs}/>
                         <input id="name" type="text" name="name" placeholder="Nome" value={inputs.name}
-                                 onChange={handleInputs}/>
+                               onChange={handleInputs}/>
                         <input id="institutionName" type="text" name="institutionName" placeholder="Instituição"
-                                 value={inputs.institutionName} onChange={handleInputs}/>
+                               value={inputs.institutionName} onChange={handleInputs}/>
 
-                        <button type="submit">Registar</button><br/>
-
+                        <button type="submit">Registar</button>
+                        <br/>
                     </form>
-
-                </div>
-        </MainLayout>
-    )
+            </div>
+</MainLayout>
+)
 }
 
 export default Register;
