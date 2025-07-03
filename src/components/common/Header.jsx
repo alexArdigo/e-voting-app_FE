fb6e0ae042ac49136e2f11f0c084e00684d9d2e6
rita_santos <rassa2@iscte-iul.pt>
2025-07-03 01:03:46 +0100

    Update header text and improve accessibility of logo images

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -10,10 +10,10 @@ export const Header = () => {
         <>
             <header>
                 <div className="logo-bar">
-                    <p>REPUBLICA PORTUGUESA</p>
+                    <p>GOVERNO DA REPÚBLICA PORTUGUESA</p>
                     <Link to={"/"}><img src="../../../public/images/Governo_de_Portugal_logo_grey.png" alt="Logo"/></Link>
-                    <Link to={"/"}><img src="../../../public/images/SGMAI_secretaria_geral.png" style={{height:"40px"}}/></Link>
-                    <p>SITE OFFICIAL VOTO ESTATISTICAS <br/>GOVERNO REPUPLICA PORTUGUES</p>
+                    <Link to={"/"}><img src="../../../public/images/SGMAI_secretaria_geral.png" style={{height:"40px"}} alt="Logo2"/></Link>
+                    <p>SITE OFICIAL<br/>VOTO ESTATÍSTICAS</p>
                 </div>
             </header>
             <div className="nav-bar">
