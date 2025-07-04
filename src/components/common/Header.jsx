81b9e2e993a817669008b69bf4a2dc7073d76d42
rita_santos <rassa2@iscte-iul.pt>
2025-07-04 13:41:40 +0100

    Update Header component to improve site title formatting

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
                     <p>GOVERNO DA REPÚBLICA PORTUGUESA</p>
                     <Link to={"/"}><img src="/images/Governo_de_Portugal_logo_grey.png" alt="Logo"/></Link>
                     <Link to={"/"}><img src="/images/SGMAI_secretaria_geral.png" style={{height: "40px"}} alt="Logo2"/></Link>
-                    <p>SITE OFICIAL<br/>VOTO ESTATÍSTICAS</p>
+                    <p>SITE OFICIAL<br/>VOTO | ESTATÍSTICAS</p>
                 </div>
             </header>
             <div className="nav-bar">
