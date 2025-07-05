60cc3edc710a3a2f60042b00b556d266d7e06509
rita_santos <rassa2@iscte-iul.pt>
2025-07-05 19:00:29 +0100

    Adjust padding of button

--- src/pages/ConfirmElectionPage.jsx
+++ src/pages/ConfirmElectionPage.jsx
@@ -31,7 +31,7 @@ const ConfirmElectionPage = () => {
         <>
             <MainLayout style={{justifyContent: "space-between"}}>
                 <div >
-                    <img src="public/images/image 4.png"></img>
+                    <img src="/images/Voting-election.png"></img>
                 </div>
 
                 <StyledContainer variant="transparent" className="dflxColumn" style={{paddingRight:"400px", marginTop: "0", paddingTop: 0}}>
