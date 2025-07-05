e9b675fb60f6745e4194cf1153e948ee1b1b8dec
rita_santos <rassa2@iscte-iul.pt>
2025-07-05 19:05:33 +0100

    Delete unused import

--- src/pages/VoteSubmittedPage.jsx
+++ src/pages/VoteSubmittedPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainLayout from "../layouts/MainLayout.jsx";
 import StyledContainer from "../components/specific/StyledContainer.jsx";
-import VotesByPartyByDistrictChart from "../components/charts/VotesByPartyByDistrictChart";
 
 const VoteSubmittedPage = () => {
     return (
