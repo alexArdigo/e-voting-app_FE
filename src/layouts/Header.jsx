b58c7b46ff032e8dee5a77ee38844638d1656b7d
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-07-06 00:56:45 +0100

    Refactor layout and styles in ContactPage, Header, MainLayout, and SelectElectionPage; adjust logo size and clean up CSS

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -17,8 +17,8 @@ export const Header = () => {
                 <div className="logo-bar">
                     <p>GOVERNO DA REPÚBLICA PORTUGUESA</p>
                     <Link to={"/"}><img src="/images/Governo_de_Portugal_logo_grey.png" alt="Logo"/></Link>
-                    <Link to={"/"}><img src="/images/SGMAI_secretaria_geral.png" style={{height: "40px"}} alt="Logo2"/></Link>
-                    <p>SITE OFICIAL<br/>VOTO | ESTATÍSTICAS</p>
+                    <Link to={"/"}><img src="/images/SGMAI_secretaria_geral.png" style={{height: "30px"}} alt="Logo2"/></Link>
+                    <p>SITE OFICIAL<br/>VOTO E ESTATÍSTICAS</p>
                 </div>
             </header>
             <div className="nav-bar">
