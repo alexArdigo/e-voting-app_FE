08a0e42f55868a775cbe3231ef2e02f7ca4efe62
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-07-06 23:21:31 +0100

    Add ResultadosLegislativasGlobaisPorAno component to ChartsContainer

--- src/components/charts/ChartsContainer.jsx
+++ src/components/charts/ChartsContainer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import VotesByPartyByDistrictChart from "./VotesByPartyByDistrictChart";
 import SampleChartForTesting from "./SampleChartForTesting";
+import ResultadosLegislativasGlobaisPorAno from "./ResultadosLegislativasGlobaisPorAno";
 
 const ChartsContainer = () => {
     return (
         <div style={{ width: "600px", margin: "0 auto" }}>
                 <VotesByPartyByDistrictChart />
             <SampleChartForTesting></SampleChartForTesting>
+            <ResultadosLegislativasGlobaisPorAno></ResultadosLegislativasGlobaisPorAno>
+
 
         </div>
     );
