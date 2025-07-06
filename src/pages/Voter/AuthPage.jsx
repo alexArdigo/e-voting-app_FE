91fb0fa9d2ea9336febd44e101a2a81e03a512b2
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-07-06 02:31:42 +0100

    Refactor layout and styles on frontend

--- src/pages/Voter/AuthPage.jsx
+++ src/pages/Voter/AuthPage.jsx
@@ -34,7 +34,7 @@ const AuthPage = () => {
     }, []);
 
     return (
-        <MainLayout style={{minHeight: "90vh"}}>
+        <MainLayout className="dflxX" style={{minHeight: "90vh"}}>
             <div className="auth-container">
                 <div className="message-box">
                     <img src="/images/cne-logo.jpg" alt="CNE"/>
