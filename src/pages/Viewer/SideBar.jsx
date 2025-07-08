bec8be645111dd97263331bffcd9e5ab6164aca2
rita_santos <rassa2@iscte-iul.pt>
2025-07-09 00:04:49 +0100

    Add userContext to Side Bar, fetch user data

--- src/pages/Viewer/SideBar.jsx
+++ src/pages/Viewer/SideBar.jsx
@@ -8,23 +8,27 @@ const SideBar = () => {
 
     return (
 
-
         <div style={{height: "100vh"}}>
 
             <MainLayout style={{ height: "100vh", width:"100vw"}}>
                 <StyledContainer variant="DefaultTransparent" style={{minWidth:"30vw"}}>
-                    <StyledContainer variant="defaultContained" className="dflxColumn" style={{heigth: "100px", width: "200px"}}>
+
+                    <StyledContainer variant="defaultContained" className="dflxColumn" style={{height: "100px", width: "200px"}}>
+
                         <img src="public/images/ProfilePic.png" height="150px" alt="Profile"/>
+
                         <div className="user-side-profile" style={{ lineHeight: 0.5 }}>
-                            <p color="green"><strong> Viewer logged in! </strong></p>
-                            <p>Last Login: 2023-10-01</p>
-                            <p >User: AnaMoura</p>
+                            <p color="green"><strong> Utilizador com sessão iniciada! </strong></p>
+                            <p> Último acesso: {user?.lastLogin || null}</p>
+                            <p> Nome: {user?.name || null}</p>
+                            <p> Nome de utilizador: {user?.username || null}</p>
+                            <p> Instituição: {user?.instituitionName || null}</p>
 
 
                         </div>
                         <div className="dflxColumn" >
                             {/*<button className="side-bar-button"> Edit profile </button>*/}
-                            <button className="side-bar-button"> Loggout </button>
+                            <button className="side-bar-button"> Sair </button>
                         </div>
 
                     </StyledContainer>
