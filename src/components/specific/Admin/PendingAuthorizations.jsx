60b2206b7871879a1e524852ed42a5c47762bbff
Sara Sousa <ssaaa3@iscte-iul.pt>
2025-07-11 12:33:02 +0100

    Refactor PendingAuthorizations component: improve search functionality and clean up code formatting

--- src/components/specific/Admin/PendingAuthorizations.jsx
+++ src/components/specific/Admin/PendingAuthorizations.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import AdminDashboard from "./AdminDashboard";
 import StyledContainer from "../../../layouts/StyledContainer";
 import api from "../../../services/api";
-import { useUserContext } from "../../../services/UserContext";
+import {useUserContext} from "../../../services/UserContext";
 
 const PendingAuthorizations = () => {
     const [pendingUsers, setPendingUsers] = useState([]);
     const [approvedUsers, setApprovedUsers] = useState([]);
-    const { user } = useUserContext();
+    const {user} = useUserContext();
     const [searchTerm, setSearchTerm] = useState("");
     const [searchTermApproved, setSearchTermApproved] = useState("");
 
     const filteredPending = pendingUsers.filter(user =>
-        user.username.toLowerCase().includes(searchTermApproved.toLowerCase())
+        user.username.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const filteredApproved = approvedUsers.filter(user =>
-        user.username.toLowerCase().includes(searchTerm.toLowerCase())
+        user.username.toLowerCase().includes(searchTermApproved.toLowerCase())
     );
 
     const fetchData = async () => {
@@ -39,16 +39,6 @@ const PendingAuthorizations = () => {
 
     const handleApprove = async (id) => {
 
-            try {
-                await api.post(`/approveViewer?id=${id}`);
-                await fetchData();
-            } catch (error) {
-                console.error("Erro ao aprovar utilizador:", error);
-            }
-    };
-
-    const handleRemove = async (id) => {
-
         try {
             await api.post(`/approveViewer?id=${id}`);
             await fetchData();
@@ -57,57 +47,77 @@ const PendingAuthorizations = () => {
         }
     };
 
+    const handleRemove = async (id) => {
+
+        try {
+            await api.post(`/deleteApprovedViewer?id=${id}`);
+            await fetchData();
+        } catch (error) {
+            console.error("Erro ao deletar utilizador:", error);
+        }
+    };
+
     useEffect(() => {
         fetchData();
         fetchApproved();
-        handleApprove();
-        handleRemove();
-
-    }, []);
+    }, [fetchApproved()]);
 
     return (
         <AdminDashboard>
             <div className="admin-container">
-            <h1>Lista de Utilizadores a Aguardar Aprovação</h1>
-            <p>Aprove o utilizador no sistema</p>
+                <h1>Lista de Utilizadores a Aguardar Aprovação</h1>
+                <p>Aprove o utilizador no sistema</p>
             </div>
             <div className="dflx">
-            <StyledContainer style={{width: "100%", marginRight: "50px"}}>
-                <h1>Pendentes de Aprovação</h1>
-                <p>Lista de utilizadores pendentes de aprovação:</p>
-
-                <input
-                    type="text"
-                    placeholder="Pesquisar utilizador..."
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}
-                    style={{ marginBottom: "10px", width: "100%" }}
-                />
-
-                <ul className="pending-users-ul">
-                    {filteredPending.map((user) => (
-                        <li className="li-pending" key={user.id}>
-                            {user.username}
-                            <button className="button-pending-list" onClick={() => handleApprove(user.id)}> Aprovar </button>
-                        </li>
-                    ))}
-                </ul>
-            </StyledContainer>
+                <StyledContainer style={{width: "100%", marginRight: "50px"}}>
+                    <h1>Pendentes de Aprovação</h1>
+                    <p>Lista de utilizadores pendentes de aprovação:</p>
+                    <input
+                        type="text"
+                        placeholder="Pesquisar utilizador..."
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                        style={{marginBottom: "10px", width: "100%"}}
+                    />
+                    <ul className="pending-users-ul">
+                        {filteredPending.map((user) => (
+                            <li className="li-pending" key={user.id}>
+                                {user.username}
+                                <button className="button-pending-list"
+                                        onClick={() => handleApprove(user.id)}> Aprovar
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </StyledContainer>
+                {/*<StyledContainer>*/}
+                {/*    <h1>Aprovados</h1>*/}
+                {/*    <p>Lista de utilizadores aprovados:</p>*/}
+                {/*    <ul>*/}
+                {/*    {approvedUsers.map((user) => (*/}
+                {/*    <li key={user.id}>*/}
+                {/*    {user.username}*/}
+                {/*    </li>*/}
+                {/*    ))}*/}
+                {/*    </ul>*/}
+                {/*    </StyledContainer>*/}
                 <StyledContainer style={{width: "100%"}}>
                     <h1>Remover utilizador</h1>
                     <p>Lista de utilizadores aprovados:</p>
                     <input
                         type="text"
                         placeholder="Pesquisar utilizador..."
-                        value={searchTerm}
-                        onChange={e => setSearchTerm(e.target.value)}
-                        style={{ marginBottom: "10px", width: "100%" }}
+                        value={searchTermApproved}
+                        onChange={e => setSearchTermApproved(e.target.value)}
+                        style={{marginBottom: "10px", width: "100%"}}
                     />
                     <ul className="pending-users-ul">
                         {filteredApproved.map((user) => (
                             <li className="li-pending" key={user.id}>
                                 {user.username}
-                                <button className="button-pending-list-remove" onClick={() => handleApprove(user.id)}> Remover </button>
+                                <button className="button-pending-list-remove"
+                                        onClick={() => handleRemove(user.id)}> Remover
+                                </button>
                             </li>
                         ))}
                     </ul>
