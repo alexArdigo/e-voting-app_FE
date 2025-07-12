93b879d6259780c373af413933d4a1403aae837b
Alexandre Ardigo <alexaardigo@gmail.com>
2025-07-12 13:42:36 +0100

    Refactor ProtectedRoute: Add user check to redirect unauthenticated users

--- src/services/ProtectedRoute.jsx
+++ src/services/ProtectedRoute.jsx
@@ -4,13 +4,17 @@ import {Navigate, useLocation} from "react-router-dom";
 
 
 const ProtectedRoute = ({children}) => {
-    const {loading} = useUserContext();
+    const {user, loading} = useUserContext();
     const location = useLocation();
 
 
     if (loading) {
         return <div>Loading...</div>;
     }
+
+    if (!user?.id)
+        return <Navigate to={"/"} replace={true}/>
+
 
     return cloneElement(children, {
         locationState: location.state
