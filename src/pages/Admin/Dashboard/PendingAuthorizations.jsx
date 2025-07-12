574da9668cb7e419cf475ba88a9367aec16abf0a
rita_santos <rassa2@iscte-iul.pt>
2025-07-12 01:25:34 +0100

    Add `fetchApproved` calls in PendingAuthorizations to ensure updated data

--- src/pages/Admin/Dashboard/PendingAuthorizations.jsx
+++ src/pages/Admin/Dashboard/PendingAuthorizations.jsx
@@ -42,6 +42,7 @@ const PendingAuthorizations = () => {
         try {
             await api.post(`/approveViewer?id=${id}`);
             await fetchData();
+            await fetchApproved();
         } catch (error) {
             console.error("Erro ao aprovar utilizador:", error);
         }
@@ -52,6 +53,7 @@ const PendingAuthorizations = () => {
         try {
             await api.post(`/deleteApprovedViewer?id=${id}`);
             await fetchData();
+            await fetchApproved();
         } catch (error) {
             console.error("Erro ao deletar utilizador:", error);
         }
