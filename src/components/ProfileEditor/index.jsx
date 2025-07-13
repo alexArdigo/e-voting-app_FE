0f3348cd397d7c2801b927bff7950f01bd469f9c
rita_santos <rassa2@iscte-iul.pt>
2025-07-13 01:07:21 +0100

    Update profile image options and improve image selection logic in ProfileEditorRefactor `ProfileEditor` component: update image options, adjust button formatting, and improve image gallery iteration logic

--- src/components/ProfileEditor/index.jsx
+++ src/components/ProfileEditor/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImages, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "./ProfileEditor.css";
 
 
 const imageOptions = [
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Medialivre.png/1024px-Medialivre.png",
+    "https://play-lh.googleusercontent.com/JZYM9BfoFZxY-NYrjmQr6BPpireEvDvcVliADoG-XpESbjQC3tu170Qjb-wgdWGwfUC3=s188-rw",
     "https://upload.wikimedia.org/wikipedia/commons/7/7b/Logo_impresa.gif",
     "https://upload.wikimedia.org/wikipedia/pt/e/e1/Media-capital-logo-certo-300x224.png",
     "https://apradiodifusao.pt/wp-content/uploads/2025/04/imagem-2.jpg"
@@ -23,8 +24,7 @@ const ProfileEditor = ({ currentImage, onSave }) => {
         <div className="profile-picture-editor">
             <img src={currentImage} alt="Profile" className="rounded-profile-image" />
             <button className="edit-pic" onClick={() => setShowPopup(true)}>
-                <FontAwesomeIcon icon={faImages} style={{ marginRight: "8px" }} />
-                Editar
+                <FontAwesomeIcon icon={faImages}/>
             </button>
 
             {showPopup && (
@@ -35,13 +35,13 @@ const ProfileEditor = ({ currentImage, onSave }) => {
                         </button>
                         <h3>Escolher nova imagem</h3>
                         <div className="image-gallery">
-                            {imageOptions.map((img, idx) => (
+                            {imageOptions.map((img, value ) => (
                                 <img
-                                    key={idx}
+                                    key={value}
                                     src={img}
                                     className={`gallery-image ${selectedImage === img ? "selected" : ""}`}
                                     onClick={() => setSelectedImage(img)}
-                                    alt={`Option ${idx}`}
+                                    alt={`Option ${value}`}
                                 />
                             ))}
                         </div>
