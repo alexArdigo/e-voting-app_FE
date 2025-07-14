2dd650276ff62bf545c435e92d70fb577d6fa745
Alexandre Ardigo <alexaardigo@gmail.com>
2025-07-14 17:57:05 +0100

    Refactor chart component structure: Simplify chart configuration and remove unused imports

--- src/pages/Voter/SelectElectionPage.jsx
+++ src/pages/Voter/SelectElectionPage.jsx
@@ -24,6 +24,7 @@ const SelectElectionPage = () => {
     const [activeElection, setActiveElection] = useState([]);
     const [voterVotedList, setVoterVotedList] = useState([]);
 
+
     useEffect(() => {
         (async () => {
             try {
