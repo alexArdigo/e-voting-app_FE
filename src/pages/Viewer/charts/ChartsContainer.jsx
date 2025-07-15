d2efd1102ad38ea2c17e7eb180e54fb4f2db13ee
tiago <tamsa2@iscte-iul.pt>
2025-07-15 05:17:57 +0100

    Update ElectedCandidates chart name for clarity in ChartsContainer

--- src/pages/Viewer/charts/ChartsContainer.jsx
+++ src/pages/Viewer/charts/ChartsContainer.jsx
@@ -10,7 +10,7 @@ import Districts from "../../../components/specific/Map/Districts";
 export const charts = [
     { type: "votesByParty", name: "Votos por Partido", component: VotesByPartyByDistrictChart },
     { type: "results", name: "Mapa de Resultados Legislativas Globais", component: Results },
-    { type: "electedCandidates", name: "Candidatos Eleitos", component: ElectedCandidates },
+    { type: "electedCandidates", name: "Candidatos Eleitos Assembleia", component: ElectedCandidates },
     { type: "globalLegislativeResults", name: "Resultados Legislativas Globais", component: GlobalLegislativeResultsPerYear },
     { type: "assemblySeats", name: "Lugares Assembleia da República", component: ElectoralSeats }
 ];
